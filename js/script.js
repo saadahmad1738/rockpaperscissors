8cc9bc5442e725ed5a48b619192b82351b9dfe2a
Saad Ahmad <s.ahmad@ycdhs.org>
2018-11-13 22:58:55 +0000

    randomNumber generator

--- js/script.js
+++ js/script.js
@@ -4,6 +4,31 @@
 
 //GLOBAL VARIABLES
 /* global $ */
+var userchoice;
+userchoice="";
+var computerChoice;
+computerChoice="";
+var winner;
+winner="";
+
+
+$("#shoot").click(function() {
+   var userchoice= $("#input").val() 
+    $("#userchoice").text(userchoice)
+   
+    var randomNumber;
+randomNumber=Math.random();
+    console.log(randomNumber);
+    if(randomNumber > .66) {
+        computerChoice = "Rock"
+    } else if (randomNumber > .33) {
+        computerChoice = "Paper" 
+    } else if (randomNumber > 0) {
+        computerChoice = "Scissor"; 
+};
+$("#computerChoice").text(computerChoice)
+});
+
 
 
 
